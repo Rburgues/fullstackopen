a7b6ce0d1121eaffc1e7c81b1034f4f188a0c4b2
Richard <rsebaburgues@gmail.com>
2024-01-25 20:58:12 -0300

    update part1

---
+++ part1/courseinfo/src/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+
+    const course = 'Half Stack application development'
+
+    return (
+      <div>
+         <h1>{course}</h1>
+      </div>
+    )
+  }
+  
+  export default Header
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,17 @@
+
+const Total = () => {
+    
+    const exercises1 = 10
+    const exercises2 = 7
+    const exercises3 = 14
+
+    return (
+        <div>
+            <p>{exercises1}</p>
+            <p>{exercises2}</p>
+            <p>{exercises3}</p>
+        </div>
+    )
+}
+
+export default Total
